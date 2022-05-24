46184c41d569334ba75026fa344c1392b7a53e0b
Jiez Tee <jieztee@gmail.com>
2022-05-24 16:38:26 +0800

    Count Number

---
+++ CountNumber/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+  data(){
+    return {
+      count:0
+    }
+  },
+  methods:{
+    increment(){
+      this.count++
+    }
+  }
+}
+</script>
+<template>
+<button @click="increment">count is: {{ count }}</button>
+</template>
